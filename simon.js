159b9f07687c6819c327f3bf749fe1f634a78768
GokulaRajan-M <156203830+GokulaRajan-M@users.noreply.github.com>
2024-03-05 18:16:04 +0530

    Add files via upload

--- simon.js
+++ simon.js
@@ -21,16 +21,18 @@ $(document).on("keypress",()=>{
 
 
 //capture the button user clicked and store
-$(".btn").on("click",function(event){
-    var userChosenColour = (event.currentTarget.id);
-    playButtonSound(userChosenColour);
-    animatePress(userChosenColour);
-    userSequence.push(userChosenColour);
-
-    CompareColors(userSequence.length - 1);
+if(started){
+    $(".btn").on("click",function(event){
+        var userChosenColour = (event.currentTarget.id);
+        playButtonSound(userChosenColour);
+        animatePress(userChosenColour);
+        userSequence.push(userChosenColour);
     
-    
-});
+        CompareColors(userSequence.length - 1);
+        
+        
+    });    
+}
 
 
 function CompareColors(position){
